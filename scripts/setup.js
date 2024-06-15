65508b1b10710c5102c346282aa6a2ec60cd5b5a
Christian Lin <christianlinlin123@gmail.com>
2024-06-15 18:38:04 -0400

    fetch resources

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,3 +2,45 @@
 window.addEventListener('load', () => {
     document.querySelector('.loading-screen').style.display = 'none';
 });
+
+/* fetching infromation from PokeAPI */
+async function fetchPokemonName() {
+    try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=898');
+    const data = await response.json();
+    return data;
+    }
+    catch (error) {
+        console.error("Error detected " + error);
+        alert("Error detected " + error)
+    }
+}
+
+/* fetching Pokemon name */
+fetchPokemonName().then( data => {
+    for (let i = 0; i < data.results.length; i++) {
+        let names = data.results[i].name;
+        console.log(names);
+    }
+});
+
+/* fetching infromation from PokeAPI */
+async function fetchPokemonType() {
+    try {
+        const responseTwo = await fetch('https://pokeapi.co/api/v2/type/');
+        const dataTwo = await responseTwo.json();
+        return dataTwo;
+    }
+    catch(error) {
+        console.error("Error detected " + error);
+        alert("Error detected " + error)
+    }
+}
+
+/* fetching Pokemon type */
+fetchPokemonType().then(dataTwo => {
+    for(let i = 0; i < dataTwo.results.length; i++) {
+        let type = dataTwo.results[i].name;
+        console.log(type);
+    }
+})
