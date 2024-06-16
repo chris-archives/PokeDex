e1ccdba9ce7a1a4613af07200a16605e5906f336
Christian Lin <christianlinlin123@gmail.com>
2024-06-15 22:41:17 -0400

    implemented better program to fetch data

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,44 +3,26 @@ window.addEventListener('load', () => {
     document.querySelector('.loading-screen').style.display = 'none';
 });
 
-/* fetching infromation from PokeAPI */
-async function fetchPokemonName() {
+/* fetching Pokemon data with PokeAPI */
+async function getPokemonData(num) {
     try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=898');
-    const data = await response.json();
-    return data;
-    }
-    catch (error) {
-        console.error("Error detected: " + error);
-        alert("Error detected: " + error)
-    }
-}
-
-/* fetching Pokemon name */
-fetchPokemonName().then( data => {
-    for (let i = 0; i < data.results.length; i++) {
-        let names = data.results[i].name;
-        console.log(names);
-    }
-});
-
-/* fetching infromation from PokeAPI */
-async function fetchPokemonType() {
-    try {
-        const responseTwo = await fetch('https://pokeapi.co/api/v2/type/');
-        const dataTwo = await responseTwo.json();
-        return dataTwo;
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
+        const data = await response.json();
+        return data;
     }
     catch(error) {
         console.error("Error detected: " + error);
         alert("Error detected: " + error)
     }
 }
+async function getAllPokemonData() {
+    let info = [];
+    let promises = [];
+    for(let i = 1; i < 500; i++) {
+        promises.push(getPokemonData(i))
+    };
+    const pokemon = await Promise.all(promises);
+    console.log(promises);
+}
+getAllPokemonData();
 
-/* fetching Pokemon type */
-fetchPokemonType().then(dataTwo => {
-    for(let i = 0; i < dataTwo.results.length; i++) {
-        let type = dataTwo.results[i].name;
-        console.log(type);
-    }
-})
